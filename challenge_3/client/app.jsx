e7da8a536d032f8fa1b3675ec3192111e32e1927
Michael Justice <m_a_justice@yahoo.com>
2020-04-23 11:57:43 -0700

    working backend

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,3 +1,12 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+class App extends ReactComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>chicken</div>
+    );
+  }
+}
 
+ReactDOM.render(<App/>, document.getElementById('app'));
