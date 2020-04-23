7e937ffc68b6dec724c0cfadff2b1fa6721e4f36
Michael Justice <m_a_justice@yahoo.com>
2020-04-23 11:57:00 -0700

    working react/webpack/express boilerplate

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 
+app.use(express.static(path.resolve('__dirname', '..', 'public')));
+app.use(express.json());
 
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.post('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`)
+});
